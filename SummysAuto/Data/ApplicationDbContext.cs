f0d0764478d507c74ffd858e1d7a276c8fc808ea
Simplevolk <simplevolk@gmail.com>
2018-03-28 22:47:49 +0300

    add migration and update database

--- SummysAuto/Data/ApplicationDbContext.cs
+++ SummysAuto/Data/ApplicationDbContext.cs
@@ -15,6 +15,8 @@ namespace SummysAuto.Data
         {
         }
 
+        public DbSet<ServiceType> ServiceTypes { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
---
+++ SummysAuto/Models/ServiceType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SummysAuto.Models
+{
+    public class ServiceType
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
