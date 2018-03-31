eff5de7c004493fcf496f412fc146a4edd475b03
Simplevolk <simplevolk@gmail.com>
2018-03-31 22:14:06 +0300

    create ServiceTypeController and add migration

--- SummysAuto/Controllers/ServiceTypesController.cs
+++ SummysAuto/Controllers/ServiceTypesController.cs
@@ -3,14 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SummysAuto.Data;
+using SummysAuto.Models;
 
 namespace SummysAuto.Controllers
 {
     public class ServiceTypesController : Controller
     {
+
+        private ApplicationDbContext _db;
+        public ServiceTypesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            return View(_db.ServiceTypes.ToList());
+        }
+
+        /// <summary>
+        /// Get Create
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        /// <summary>
+        /// Post ServiceTypes/Create
+        /// </summary>
+        public async Task<IActionResult> Create(ServiceType serviceType)
+        {
+            if (!ModelState.IsValid)
+                return View(serviceType);
+
+            _db.Add(serviceType);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+                _db.Dispose();
         }
     }
 }
--- SummysAuto/Data/Migrations/20180331191247_updateFieldsInServiceType.Designer.cs
+++ SummysAuto/Data/Migrations/20180331191247_updateFieldsInServiceType.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace SummysAuto.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180331191247_updateFieldsInServiceType")]
+    partial class updateFieldsInServiceType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
